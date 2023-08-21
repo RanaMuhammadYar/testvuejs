c780f92bc697e7478f4619b3ed0c9d45c28187ab
Muhammad Yar <96819805+RanaMuhammadYar@users.noreply.github.com>
2023-08-21 12:52:17 +0500

    change

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <H2 class="text-center  mt-5">
-        This is Vue page and this is update file
+        This is Vue page and this is update file new  upate
         </H2>
     </div>
 </template>
